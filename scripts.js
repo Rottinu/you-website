3fc39de9d6fd5d2103214466da3da91b41f682a3
Rottinu <rottinu.crypto@gmail.com>
2025-01-28 21:07:16 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -49,3 +49,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+// Change header background on scroll
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled'); // Add class when scrolling down
+    } else {
+        header.classList.remove('scrolled'); // Remove class when back to top
+    }
+});
+
+
